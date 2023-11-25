38bee08a62b6f09c7214e6f1b3bddbde5aa6a19e
jesusdavidlm3 <jesusdavidlozanomarin@gmail.com>
2023-11-25 16:17:07 -0400

    correcciones
    corregida la alineacion y distribucion de elementos del segundo panel de informacion en la pagina principal, se escondera el excedente horizontal pero se mantendra el vertical en scroll para su desplazamiento

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,20 +1,22 @@
 export const StartPage = () => {
     return(
         <div className="startPage">
-            {/* <img className="welcomeLogo" src="../src/img/LozanoTechIsotipoBlue.png"/>
+            <img className="welcomeLogo" src="../src/img/LozanoTechIsotipoBlue.png"/>
             <h1>Hola!</h1>
             <p>
                 Somos una organizacion pequeña dedicada a llevar a cabo reparaciones
-                reparaciones accesibles y de calidad para equipos de escritorio y laptops para
+                accesibles y de calidad en equipos de escritorio y laptops para
                 hogares, pequeñas empresas y estudiantes.
-            </p> */}
+            </p>
 
             <div className="infoContainer1">
                 <img src="./src/img/ref1.png"/>
                 <div>
                     <h1>Tu tiempo vale oro</h1>
                     <p>Nos enfocamos en ofrecerte toda la calidad de nuestro servicio
-                        siempre en tiempo record
+                        siempre en tiempo record ya que sabemos que necesitas tu equipo
+                        para continuar con tu trabajo o simplemente quieres disfrutar
+                        de el y estamos concientes de ello.
                     </p>
                 </div>
             </div>
