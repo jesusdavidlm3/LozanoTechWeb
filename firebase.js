4e9750dc77a7b71521c339ba65e8a95cad64b77e
jesusdavidlm3 <jesusdavidlozanomarin@gmail.com>
2023-12-01 19:53:03 -0400

    Avances
    Añadida la base para utilizar firebase en para las consultas

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    piKey: "AIzaSyBbs0LfIVQH2OOidkAwOcrB2RUIptyI1TI",
+    authDomain: "lozano-tech-web.firebaseapp.com",
+    projectId: "lozano-tech-web",
+    storageBucket: "lozano-tech-web.appspot.com",
+    messagingSenderId: "444506054667",
+    appId: "1:444506054667:web:df23cb724f38e5dce1e70e",
+    measurementId: "G-6VMF5614LL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
