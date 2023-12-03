485320598ae7b9196c09e36b12baf31db42aa5b1
jesusdavidlm3 <jesusdavidlozanomarin@gmail.com>
2023-12-02 23:26:13 -0400

    Avances
    ya es posible leer de firebase, la escitura esta deshabilitada. falta enseñar los resultados de la lectura configurar una lectura segun el numero de referencia captado

--- src/components/CheckPage.jsx
+++ src/components/CheckPage.jsx
@@ -1,15 +1,14 @@
 import { Button } from "./Button/Button"
 import { TextField } from "./TextField/TextField"
+import { doc, getDoc } from "firebase/firestore"
 import { db } from "../../firebase"
 
 export const CheckPage = () => {
 
-    async function handleSubmit(){
-        try{
-            const device = await db.collection('repairStatus').document('1').get();
-        }catch{
-            console.log('parece que algo salio mal')
-        }
+    async function checkStatus(){
+        const docRef = doc(db, 'repairStatus', '1');
+        const docSnap = await getDoc(docRef);
+        console.log(docSnap.data());
     }
 
 
@@ -18,18 +17,18 @@ export const CheckPage = () => {
         <div className="checkBackground">
             <div className="mainCheckContainer">
                 <div className="checkContainer1">
-                    <form className="Form">
-                        <TextField id='controlNumber' label='Ingrese aqui su numero de control'></TextField>
-                        <Button onClick={handleSubmit} variant='allow' label='Consultar'></Button>
-                    </form>
+                    <div className="Form">
+                        <TextField id='controlNumber' label='Numero de control'></TextField>
+                        <Button onClick={checkStatus} variant='allow' label='Consultar'></Button>
+                    </div>
                 </div>
 
                 <div className="checkContainer2">
                     <div className="checkedInfo">
                         <h3>Su equipo a reparar es:</h3>
-                        <p>{device.device}</p>
+                        {/* <p>{device.device}</p> */}
                         <h3>Su estado actual es:</h3>
-                        <p>{device.state}</p>
+                        {/* <p>{device.state}</p> */}
                     </div>
                 </div>
             </div>
