0484cf4296cfaed123b031980b7de1634eec9d9a
jesusdavidlm3 <jesusdavidlozanomarin@gmail.com>
2023-12-20 18:21:51 -0400

    Avances
    Implementado menu de navegacion para dispostivos moviles, falta dar estilos al menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
+import { useState, useNavigate } from 'react'
 import NavBar from './components/NavBar'
 import Page from './components/Page'
 import './style.scss'
 
 function App() {
 
-  const [display, setDisplay] = useState('start')
+  const [display, setDisplay] = useState('start');
+  // const navigate = useNavigate();
 
   return (
     <div className='container'>
       <NavBar
+        // onclickMenu={ () =>  }
         onClickLogin={ () => setDisplay('log') }
         onClickCheck={ () => setDisplay('check')}
         onClickStart={ () => setDisplay('start')}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,28 @@
 import { Button } from "./Button/Button";
+import { useState } from "react";
 
-const NavBar = ({onClickLogin, onClickCheck, onClickStart, onClickServices, onClickAboutUs, onClickComments}) => {
+const NavBar = ({onClickLogin, onClickCheck, onClickStart, onClickServices, onClickAboutUs, onClickComments, onClickMenu}) => {
+
+    const [displayMenu, setDisplayMenu] = useState(false)
+
     return(
         <div className='NavBar'>
-            <img className="burguerMenuIcon" src="../src/img/icons/menu.png"/>
+
+            { displayMenu && 
+                <div className="menu">
+                    <img className="burguerMenuIcon" src="../src/img/icons/menu.png" onClick={ () => {setDisplayMenu(!displayMenu)}}/>
+                    <h3 onClick={onClickStart}>Inicio</h3>
+                    <h3 onClick={onClickServices}>Servicios</h3>
+                    <h3 onClick={onClickComments}>Comentarios</h3>
+                    <h3 onClick={onClickAboutUs}>Sobre nosotros</h3>
+                </div> 
+            }
+
+            { !displayMenu && 
+                <img className="burguerMenuIcon" src="../src/img/icons/menu.png" onClick={ () => {setDisplayMenu(!displayMenu)}}/>
+            }
+            
+
             <div className="info">
                 <img src="../src/img/LozanoTechIsotipoWhite.png" className="logo"/>
                 <h3 onClick={onClickStart}>Inicio</h3>
