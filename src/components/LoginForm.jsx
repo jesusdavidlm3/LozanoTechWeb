7d76c7cca8ddd6a6aa864d1463876062aed19b10
jesusdavidlm3 <jesusdavidlozanomarin@gmail.com>
2023-12-27 18:52:54 -0400

    Cambios
    se empezo a agregar el soportte para hacer cambios en la base de datos

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,23 @@
+import { useState } from 'react'
 import { Button } from "./Button/Button";
 import { TextField } from "./TextField/TextField";
 
 const LoginForm = () => {
+
+    const [signed, setSigned]=useState(false)
+
     return(
         <>
+            {signed ? (
+                <h1>hola</h1>
+            ):(
             <form className="Form">
                 <h1>Iniciar sesion</h1>
                 <TextField label='Identificacion' type="number"></TextField>
                 <TextField type="password" label='Contraseña'></TextField>
 
                 <Button type="submit" variant='allow' label='iniciar Sesion'></Button>
-            </form>
+            </form>)}
         </>
     )
 }
