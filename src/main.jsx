ef46ace5d08de434e054a81223521a2432ec95ad
jesusdavidlm3 <jesusdavidlozanomarin@gmail.com>
2024-01-01 16:18:04 -0400

    Avances
    Agregada una pagina para manejar los errores en el enrutamiento que ya empieza a funcionar.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom"
+import ErrorPage from './routes/ErrorPage.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <>
+    <RouterProvider router={router}/>
+  </>
 )
---
+++ src/routes/ErrorPage.jsx
@@ -0,0 +1,11 @@
+export default function ErrorPage() {
+    return(
+        <div className="errorPageBack">
+            <div className="errorPageContainer">
+                <h1>Oops!</h1>
+                <h3>La pagina a la que intentas acceder no existe.</h3>
+                <h3>Por favor intenta con otra direccion o enlace.</h3>
+            </div>
+        </div>
+    )
+}
